68a28f3af6bd46f89b64aa2da6f173011632d88d
Mathijs van Bockhooven <m.athijs@hotmail.com>
2015-11-13 16:05:59 +0100

    first page type added

---
+++ EPiTechnoSessie/Models/Pages/HomePage.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EPiTechnoSessie.Models.Pages
+{
+    [ContentType(DisplayName = "HomePage", GUID = "0860b0e9-d9a4-46f8-a714-20eef476c195", Description = "The home page of the website")]
+    public class HomePage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
