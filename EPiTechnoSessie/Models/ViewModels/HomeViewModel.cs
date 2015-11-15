d5c112349ab87cfa868d06d892311617f640507f
Mathijs van Bockhooven <m.athijs@hotmail.com>
2015-11-15 15:24:06 +0100

    HomeViewModel added

---
+++ EPiTechnoSessie/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using EPiTechnoSessie.Models.Pages;
+using System.Collections.Generic;
+
+namespace EPiTechnoSessie.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public HomePage CurrentPage { get; set; }
+        public IEnumerable<StandardPage> ChildPages { get; set; }
+    }
+}
